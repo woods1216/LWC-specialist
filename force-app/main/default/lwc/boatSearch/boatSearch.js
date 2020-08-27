ea8a9c39839d8665c8207f858a142357971270c2
First Last <you@email.com>
2020-08-26 20:17:35 -0500

    boatSearch

--- force-app/main/default/lwc/boatSearch/boatSearch.js
+++ force-app/main/default/lwc/boatSearch/boatSearch.js
@@ -1,18 +1,38 @@
 import { LightningElement } from 'lwc';
-
-export default class BoatSearch extends LightningElement {
+import { NavigationMixin } from 'lightning/navigation';
+export default class BoatSearch extends NavigationMixin(LightningElement) {
     isLoading = false;
+    //boatTypeId = '';
   
     // Handles loading event
-    handleLoading() { }
+    handleLoading(event) { 
+        this.template.addEventListener('beforeunload', () => {
+            this.isLoading = true;
+          });
+    }
     
     // Handles done loading event
-    handleDoneLoading() { }
+    handleDoneLoading(event) {
+        this.template.addEventListener('load', () => {
+            this.isLoading = false;
+          });
+     }
     
     // Handles search boat event
     // This custom event comes from the form
-    searchBoats(event) { }
+    searchBoats(event) {
+        const boatTypeId = event.detail.boatTypeId;
+        this.template.querySelector("c-boat-search-results").searchBoats(boatTypeId);
+     }
     
-    createNewBoat() { }
+    createNewBoat() {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__objectPage',
+            attributes: {
+                objectApiName: 'Boat__c',
+                actionName: 'new',
+            },
+        });
+     }
 
 }
